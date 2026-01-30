052111a5fb739c19ae48055810c6f64016abeeb1
Fernandoow <fernando.designerartefinal@gmail.com>
2026-01-29 21:45:49 -0300

    2
    .

---
+++ index.php
@@ -0,0 +1,20 @@
+<html lang="PT-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/style.css">
+    <title>Page1</title>
+</head>
+<body>
+    <header>
+    <ul class="navegação">
+            <li><a href="index.html">Home</a></li>
+            <li><a href="produtos_page.html">Produtos</a></li>
+            <li><a href="login_page.html">Login</a></li>
+            <li><a href="cadastro_page.html">Cadastro</a></li>
+            <li><a href="cadastro_vendedor.html">Seja um vendedor</a></li>
+            <li><a href="cadastra_produto.html">Cadastro de produto</a></li>
+        </ul>
+    </header>
+</body>
+</html>
